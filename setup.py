170d1d44dc67e4d91ae71ddf012c62c93fc2a058
Brenda Javornik <brenda@eol-albireo.local>
2022-03-11 15:54:50 -0700

    messing around with the navigation

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import setuptools
 
-setuptools.setup(
-    name="my-custom-django-app",
-    version="0.0.1",
-    description="LROSE Workflows",
-    packages=setuptools.find_packages(),
-    install_requires=[
-        'django>=1.11.16'
-    ],
-    entry_points="""
-[airavata.djangoapp]
-custom_app = custom_app.apps:CustomAppConfig
-[airavata.output_view_providers]
-hawkeye-ppi-viewer = custom_app.output_views:GaussianEigenvaluesViewProvider
-""",
-)
+setuptools.setup()
+    #name="my-custom-django-app",
+    #version="0.0.1",
+    #description="LROSE Workflows",
+    #packages=setuptools.find_packages(),
+    #install_requires=[
+        #'django>=1.11.16'
+    #],
+    #entry_points="""
+#[airavata.djangoapp]
+#custom_app = custom_app.apps:CustomAppConfig
+#[airavata.output_view_providers]
+#hawkeye-ppi-viewer = custom_app.output_views:GaussianEigenvaluesViewProvider
+#""",
+#)
